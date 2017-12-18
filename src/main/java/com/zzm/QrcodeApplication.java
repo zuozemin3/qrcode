69d9e5f8fc5abf3fa7390dfadf68eaa906750339
kris <ZUOZEMIN@ECEJ.COM>
2017-12-18 15:35:48 +0800

    first commit

---
+++ src/main/java/com/zzm/QrcodeApplication.java
@@ -0,0 +1,15 @@
+package com.zzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kris
+ */
+@SpringBootApplication
+public class QrCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeApplication.class, args);
+	}
+}
